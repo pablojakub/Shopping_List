d8f7837d2ef98c483172feb78a1b61f940bc9108
jakubowskip <pawel.jakubowski92@gmail.com>
2022-09-29 10:41:01 +0200

    Added Global styles and header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Header } from './components/Header/Header';
+import GlobalStyles from './GlobalStyles';
 
 function App() {
   return (
     <>
     <Header/>
+    <GlobalStyles/>
     </>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 5rem;
+    padding: 0 10%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #252424;
+`;
+
+const Title = styled.h1`
+    color: white;
+`;
+
+const TotalPriceWrapper = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+`;
 
 export const Header: React.FunctionComponent = () => {
-  return (
-    <div>: React</div>
-  )
-}
+    return (
+        <HeaderWrapper>
+            <Title>Shopping List</Title>
+            <TotalPriceWrapper></TotalPriceWrapper>
+        </HeaderWrapper>
+    );
+};
