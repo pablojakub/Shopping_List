617a2a4b829a5cd16fbbc7b781efe2113ce76af3
jakubowskip <pawel.jakubowski92@gmail.com>
2022-10-03 10:52:45 +0200

    Added TotalPrice component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { TotalPrice } from "./TotalPrice";
 
 const HeaderWrapper = styled.header`
     width: 100%;
@@ -11,21 +12,27 @@ const HeaderWrapper = styled.header`
     background-color: #252424;
 `;
 
-const Title = styled.h1`
+const Title = styled.h2`
     color: white;
+    font-size: clamp(1rem,
+    1.5vw + 1rem,
+    2.5rem);
 `;
 
 const TotalPriceWrapper = styled.ul`
     list-style: none;
     margin: 0;
     padding: 0;
+    height: 50%;
 `;
 
 export const Header: React.FunctionComponent = () => {
     return (
         <HeaderWrapper>
             <Title>Shopping List</Title>
-            <TotalPriceWrapper></TotalPriceWrapper>
+            <TotalPriceWrapper>
+                <TotalPrice/>
+            </TotalPriceWrapper>
         </HeaderWrapper>
     );
 };
