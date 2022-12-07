260b255e0866fead8a8f66ada42cfe0f97586c0a
jakubowskip <pawel.jakubowski92@gmail.com>
2022-12-07 14:26:41 +0100

    Added Tooltip and animations

--- components/ShoppingList/AddUnknownProductComponent/AddUnknownProductComponent.styled.ts
+++ components/ShoppingList/AddUnknownProductComponent/AddUnknownProductComponent.styled.ts
@@ -1,6 +1,38 @@
 import { Wrapper } from "../../../styles/ShoppingListItem.styled"
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
+
+const spin = keyframes`
+    from {
+        transform:rotate(0deg);
+    }
+    to {
+        transform:rotate(360deg);
+    }
+`
+export const TooltipText = styled.span`
+    visibility: hidden;
+    width: 200px;
+    background-color: #252424;
+    color: white;
+    text-align: center;
+    padding: 5px 0;
+    border-radius: 6px;
+    position: absolute;
+    transform: translate(100px, -50px);
+    z-index: 1;
+    font-size: 0.7rem;
+`
 
 export const NewProductWrapper = styled(Wrapper)`
     background-color: '#1ad1b9';
+
+    &:hover {
+        ${TooltipText} {
+            visibility: visible;
+        }
+    }
+
+    > svg:hover {
+        animation: ${spin} 300ms ease-in-out;
+    }
 `
--- components/ShoppingList/AddUnknownProductComponent/AddUnknownProductComponent.tsx
+++ components/ShoppingList/AddUnknownProductComponent/AddUnknownProductComponent.tsx
@@ -1,12 +1,12 @@
-import { NewProductWrapper } from "./AddUnknownProductComponent.styled";
+import { NewProductWrapper, TooltipText } from "./AddUnknownProductComponent.styled";
 import React from 'react';
 
 
 const AddUnknownProductComponent: React.FC = () => {
     return (
         <NewProductWrapper isAdded> 
-            <p> yeah</p>
-
+            <svg xmlns="http://svgjs.com/svgjs" xmlnsXlink="http://www.w3.org/1999/xlink" width="60" height="60"><svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 16 16"><path fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 1v14M1 8h14" className="colorStroke000 svgStroke"/></svg></svg>
+            <TooltipText>Didn't found item? Add new one</TooltipText>
         </NewProductWrapper>
     )
 }
